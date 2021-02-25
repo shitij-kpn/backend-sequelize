43cc0ea2fc871d4223e6284942afc11e26a63d6e
shitij <shitijtalan@gmail.com>
2021-02-25 15:16:40 +0530

    force: true

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const { sequelize } = require("./models");
 
 app.listen(5000, async () => {
   console.log("Running on port 5000");
-  await sequelize.sync();
+  await sequelize.sync({ force: true });
   console.log("database connected");
 });
